9d06317d369bc30fbd1ddb701f5150926b9c8078
NotBB <notice76610@gmail.com>
2023-05-02 11:52:20 +0200

    Update 1.js

---
+++ assets/js/1.js
@@ -0,0 +1,19 @@
+// Création du tableau contenant les mois de l'année
+var mois = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
+  ];
+  
+  // Affichage du tableau dans la console
+  console.log(mois);
+  
