91dc56e0ed5fa5f5516bb9429576c2065ec266b7
NotBB <notice76610@gmail.com>
2023-05-02 13:27:38 +0200

    Create 3.js

---
+++ assets/js/3.js
@@ -0,0 +1,11 @@
+var tab=new Array();
+tab[0]="Lundi";
+tab[1]="Mardi";
+tab[2]="Mercredi";
+tab[3]="Jeudi";
+tab[4]="Vendredi";
+tab[5]="Samedi";
+tab[6]="Dimanche";
+var longueur=tab.length;
+
+console.table(tab[5]);
