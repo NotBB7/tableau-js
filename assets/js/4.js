cf24a039c2c32e2fa77ad1ce39c5152015ea6c03
NotBB <notice76610@gmail.com>
2023-05-02 13:30:45 +0200

    Create 4.js

---
+++ assets/js/4.js
@@ -0,0 +1,16 @@
+var tab=new Array();
+tab[1]="Janvier";
+tab[2]="Fevrier";
+tab[3]="Mars";
+tab[4]="Avril";
+tab[5]="Mai";
+tab[6]="Juin";
+tab[7]="Juillet";
+tab[8]="Aout";
+tab[9]="Septembre";
+tab[10]="Octobre";
+tab[11]="Novembre";
+tab[12]="Décembre";
+var longueur=tab.length;
+
+console.table(tab);
