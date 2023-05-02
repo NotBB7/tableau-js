d203571bc2fc76b2a451dd6b333cfdca3f1143ac
NotBB <notice76610@gmail.com>
2023-05-02 13:00:29 +0200

    Update 2.js

--- assets/js/2.js
+++ assets/js/2.js
@@ -6,3 +6,11 @@ tab[3] = "Jeudi";
 tab[4] = "Vendredi";
 tab[5] = "Samedi";
 tab[6] = "Dimanche";
+var longueur = tab.length;
+
+console.table(tab);
+
+console.table(tabPage);
+var j=1;
+document.write( "Le jour à l'indice " + j + " est " + tab[j] );
+document.write( ". C'est un jour de " + tabPage[ tab[j] ] );
